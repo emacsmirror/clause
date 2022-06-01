0388a7ea8fd013d62542777291a973e48cb522dd
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2022-06-01 16:16:36 +0200

    initial commit

---
+++ clause.el
@@ -0,0 +1,146 @@
+;;; clause.el --- Clause functions -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Marty Hiatt
+;; Author: Marty Hiatt <martianhiatus [a t] riseup [dot] net>
+;; Keywords: wp, convenience, sentences
+;; Version: 0.1
+;; URL: https://codeberg.org/martianh/clause.el
+;; Package-Requires: ((emacs "27.1") (segment "0.1") (mark-thing-at "0.3"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Functions for moving, marking and killing by clause.
+;; For now a clause is delimited by , ; : ( ) and –.
+
+;;; Code:
+(require 'segment)
+(require 'mark-thing-at)
+
+(defgroup clause nil
+  "Clause functions."
+  :group 'convenience)
+
+(defcustom clause-use-segment t
+  "Whether to use `segment' to determine clause and sentence endings."
+  :group 'clause)
+
+(defcustom clause-handle-org-footnotes nil
+  "Whether to handle org-footnotes."
+  :group 'clause)
+
+(defcustom clause-extra-delimiters nil
+  "Extra characters to add to the clause-ending regex.")
+
+(defvar clause-forward-sentence-function
+  (if clause-use-segment
+      'segment-forward-sentence
+    'forward-sentence))
+
+(defvar clause-backward-sentence-function
+  (if clause-use-segment
+      'segment-backward-sentence
+    'backward-sentence))
+
+(defvar clause-simplified-org-footnote-re
+  "\\(\\[fn:[-_[:word:]]+\\]\\)?")
+
+(defun clause-forward-sentence (&optional arg)
+  "Call `clause-forward-sentence-function' with ARG."
+  (apply clause-forward-sentence-function (list arg)))
+
+(defun clause-backward-sentence (&optional arg)
+  "Call `clause-backward-sentence-function' with ARG."
+  (apply clause-backward-sentence-function (list arg)))
+
+(defun clause--sentence-end-base-clause-re ()
+  "Return a `sentence-end-base' type regex with clause-ending characters added."
+  (concat "[)]*" ; poss closing parens prior to clause char
+          "[" ; opening of sentence-end-base regex
+          ",;:—)" ; our separators
+          clause-extra-delimiters
+          (substring sentence-end-base 1)
+          (when clause-handle-org-footnotes
+            clause-simplified-org-footnote-re)))
+
+(defun clause--opening-paren-in-sentence ()
+  "Go to next opening paren within the reach of `clause-forward-sentence'.
+Returns the position just after the opening paren."
+  (re-search-forward "("
+                     ;; limit search:
+                     (save-excursion (clause-forward-sentence)
+                                     (point))
+                     t)) ; nil when nothing found
+
+(defun clause-forward-clause (&optional arg)
+  "Move forward to beginning of next clause.
+With ARG, do this that many times."
+  (interactive "p")
+  (let ((sentence-end-base (clause--sentence-end-base-clause-re)))
+    (dotimes (_count (or arg 1))
+      (or (unless (looking-at "(") ; prevent single ( being treated as clause
+            (clause--opening-paren-in-sentence))
+          (clause-forward-sentence)))))
+
+(defun clause-backward-clause (&optional arg)
+  "Move backward to beginning of current clause.
+With ARG, do this that many times."
+  (interactive "p")
+  (let ((sentence-end-base (clause--sentence-end-base-clause-re)))
+    (dotimes (_count (or arg 1))
+      (or (re-search-backward "("
+                              ;; limit search:
+                              (save-excursion (clause-backward-sentence)
+                                              (point))
+                              t)
+          (clause-backward-sentence)))))
+
+(defun clause-kill-to-clause (&optional arg)
+  "Kill text up to the next clause.
+With ARG, do so that many times."
+  (interactive "p")
+  (let ((sentence-end-base (clause--sentence-end-base-clause-re)))
+    (dotimes (_count (or arg 1))
+      (if (save-excursion (clause--opening-paren-in-sentence))
+          (zap-up-to-char 1 (string-to-char "("))
+        (kill-sentence)))))
+
+(defun clause-kill-current-clause (&optional arg)
+  "Kill the current clause.
+With ARG, do so that many times."
+  (interactive "p")
+  (let ((sentence-end-base (clause--sentence-end-base-clause-re)))
+    (clause-backward-clause)
+    (dotimes (count (or arg 1))
+      (if (clause--opening-paren-in-sentence)
+          (zap-up-to-char 1 (string-to-char "("))
+        (kill-sentence)))))
+
+(defun clause-mark-clause ()
+  "Mark current clause."
+  (interactive "p")
+  (let ((sentence-end-base (clause--sentence-end-base-clause-re)))
+    (mark-sentence))) ; ext dep
+
+(defun clause-mark-to-clause (&optional arg)
+  "Mark from point to end of current clause.
+With ARG, do so that many times."
+  (interactive "p")
+  (let ((sentence-end-base (clause--sentence-end-base-clause-re)))
+    (mark-end-of-sentence arg)))
+
+
+(provide 'clause)
+;;; clause.el ends here
